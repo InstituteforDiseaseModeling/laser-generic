b62db68617099bdb993e3fbc67822297e8731a3e
Jonathan Bloedow <jonathan.bloedow@gatesfoundation.org>
2025-10-17 16:42:14 -0700

    Showing how with some very little wrappers we can get all relevant l-c doc strings in l-g docs.

--- src/laser_generic/wrappers.py
+++ src/laser_generic/wrappers.py
@@ -1,8 +1,16 @@
 from laser_core.laserframe import LaserFrame
 from laser_core.propertyset import PropertySet
+from laser_core.demographics import AliasedDistribution
+from laser_core.demographics import KaplanMeierEstimator
 
 class LGLaserFrame(LaserFrame):
     pass
 
 class LGPropertySet(PropertySet):
+    pass
+
+class LGAliasedDistribution(AliasedDistribution):
+    pass
+
+class LGKaplanMeierEstimator(KaplanMeierEstimator):
     pass
